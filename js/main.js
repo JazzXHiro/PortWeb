518731fe842c25b06e075a34464f698f566b79b0
JazzXHiro <73639405+JazzXHiro@users.noreply.github.com>
2026-01-07 03:27:05 +0530

    refined the home page and added About page

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ const navMenu = document.getElementById('nav-menu'),
 /*===== MENU SHOW =====*/
 if(navToggle){
     navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('show-menu') /* override top -120% to 0 */
+        // when clicked, add the show-menu class to navMenu
     })
 }
 
@@ -15,4 +16,52 @@ if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
-}   
+}
+
+/*======================== REMOVE MENU MOBILE ======================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*======================== HOME TYPED JS ======================*/
+const typedHome = new Typed('#home-typed', {
+    strings: ['<i>Web Developer</i>', '<i>Game Developer</i>', "<i>CS Student</i>"], //Insert professions
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: '_',
+})
+
+/*======================== Discord ID COPY ======================*/
+function copyDiscord(event) {
+  event.preventDefault(); // stops page jump
+
+  const discordUsername = "cyberjazz_cj";
+  //const msg = document.getElementById("discord-msg");
+  const icon = document.getElementById("discord-icon");
+
+  navigator.clipboard.writeText(discordUsername).then(() => {
+    //msg.textContent = "Discord username copied!";
+    icon.classList.remove("ri-discord-fill");
+    icon.classList.add("ri-link");
+    
+    setTimeout(() => {
+      //msg.textContent = "";
+      icon.classList.remove("ri-link");
+      icon.classList.add("ri-discord-fill");
+    }, 1500);
+  });
+}
+
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    // Add a class if the bottom of the header is more than 50px from the top of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
